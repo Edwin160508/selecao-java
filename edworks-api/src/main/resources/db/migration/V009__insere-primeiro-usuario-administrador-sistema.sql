fcfe164e7d114b6299f21f7d78f76b3540181f50
Edwin Lima <edwin.limaa@gmail.com>
2020-04-25 16:00:22 -0300

    Adicionando dependencia SpringSecurity, adicionando 1 usuario com perfil administrador no sistema.

---
+++ edworks-api/src/main/resources/db/migration/V009__insere-primeiro-usuario-administrador-sistema.sql
@@ -0,0 +1,10 @@
+INSERT INTO usuario VALUES
+(1,'admin','edwin.limaa@gmail.com','$2a$10$9LGqLlrPJQQmuXoA7l9wtOZdy115CnJA2QV96.CRKe.1FRVwSd8ny');
+
+INSERT INTO usuario_permissao VALUES
+(1,1),
+(1,2),
+(1,3),
+(1,4),
+(1,5),
+(1,6);
