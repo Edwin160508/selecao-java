309f62f309687716117252b1f0fa15f1daa5d883
Edwin Lima <edwin.limaa@gmail.com>
2020-05-24 11:15:16 -0300

    Montando Conteudo e Rodape, criando component home

--- edworks-client/src/app/template/template.module.ts
+++ edworks-client/src/app/template/template.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent, SidebarComponent],
+  declarations: [NavbarComponent, SidebarComponent, HomeComponent],
   imports: [
     CommonModule
   ],
   exports:[
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    HomeComponent
   ]
 })
 export class TemplateModule { }
