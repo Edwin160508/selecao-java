3de8c001452e80f92d6d3084f4e4380102ae3d5f
Edwin Lima <edwin.limaa@gmail.com>
2020-05-24 17:45:19 -0300

    Usando FormsModule para capturar dados vindos do formulário

--- edworks-client/src/app/usuario/usuario-form/usuario-form.component.ts
+++ edworks-client/src/app/usuario/usuario-form/usuario-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../usuario';
 
 @Component({
   selector: 'app-usuario-form',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuarioFormComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario;
+  constructor() { 
+    this.usuario = new Usuario();
+  }
 
   ngOnInit(): void {
+  }
+
+  salvar(){
+    console.log(this.usuario);
+    console.log(this.usuario.id);
+    console.log(this.usuario.nome);
+    console.log(this.usuario.email);
+    console.log(this.usuario.senha);
+    console.log(this.usuario.perfil);
   }
 
 }
--- edworks-client/src/app/usuario/usuario.module.ts
+++ edworks-client/src/app/usuario/usuario.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { UsuarioRoutingModule } from './usuario-routing.module';
 import { UsuarioFormComponent } from './usuario-form/usuario-form.component';
@@ -9,7 +10,8 @@ import { UsuarioFormComponent } from './usuario-form/usuario-form.component';
   declarations: [UsuarioFormComponent],
   imports: [
     CommonModule,
-    UsuarioRoutingModule
+    UsuarioRoutingModule,
+    FormsModule
   ],
   exports:[UsuarioFormComponent]
 })
