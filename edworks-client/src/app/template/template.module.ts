b4cc456e4d9a0b6959d52df749c937b64366c0cb
Edwin Lima <edwin.limaa@gmail.com>
2020-05-24 11:23:55 -0300

    Movendo Component Home para pasta raiz

--- edworks-client/src/app/template/template.module.ts
+++ edworks-client/src/app/template/template.module.ts
@@ -2,19 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { HomeComponent } from './home/home.component';
+
 
 
 
 @NgModule({
-  declarations: [NavbarComponent, SidebarComponent, HomeComponent],
+  declarations: [NavbarComponent, SidebarComponent],
   imports: [
     CommonModule
   ],
   exports:[
     NavbarComponent,
-    SidebarComponent,
-    HomeComponent
+    SidebarComponent
   ]
 })
 export class TemplateModule { }
